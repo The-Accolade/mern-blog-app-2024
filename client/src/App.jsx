bcd5a0cd3e642c9bf5c77a7a987f161db4886a33
The-Accolade <olusolaakolade@gmail.com>
2024-04-24 19:09:08 +0100

    Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -18,5 +20,5 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,29 @@
+// eslint-disable-next-line no-undef
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: "#174694",
+        secColor: "#934C9D",
+        tertColor: "#DE5A8D",
+        dark: "#444655",
+        light: "#F1F1E6"
+      },
+      fontFamily: {
+        mainFont: ["Open Sans", "sans-serif"]
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
 
