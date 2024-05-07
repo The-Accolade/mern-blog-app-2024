c17b51d4a61bf5a5b2f01329729f797a6983eb85
The-Accolade <olusolaakolade@gmail.com>
2024-05-07 14:04:48 +0100

    Login Route backend Added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -24,4 +25,37 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || password === "" || email === "") {
+       return next(errorHandler(400, "Enter your username and password"));
+    }
+
+    try {
+       const validUser = await User.findOne({ email });
+       if(!validUser) {
+        next(errorHandler(404, "User not found"));
+       }
+       const validPassword = await bcryptjs.compareSync(password, validUser.password);
+       if(!validPassword) {
+        return next(errorHandler(400, "Incorrect Password"));
+       }
+
+       const token = await jwt.sign(
+        {id: validUser._id}, process.env.JWT_SECRET
+       );
+
+       console.log(validUser);
+
+       const {password: pass, ...rest} = validUser._doc;
+
+       res.status(200).cookie("Access token", token, {
+        httpOnly: true
+       }).json(rest);
+    } catch (error) {
+        next(error);
+    }
 }
